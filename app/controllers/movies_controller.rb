e6f60c80bbd7cb31c3d97cadb0fe59b0f8a7f7a0
Samuel James <bsbcraftplays@gmail.com>
2021-08-30 22:57:34 -0400

    Filtering the list of movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,28 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
   def index
-    sort_by = params[:sort_by]
-    if (sort_by == 'title') then
-      @movies = Movie.all.order(title: :asc)
-    elsif (sort_by == 'release_date') then
-      @movies = Movie.all.order(release_date: :asc)
+    @sort_by = params[:sort_by]
+    @ratings = params[:ratings]
+    @movies = Movie.all
+
+    if (@ratings != nil) then
+      if (defined?(@ratings.keys)) then
+        @ratings = @ratings.keys
+      end
+      @movies = @movies.where(:rating => @ratings)
     else
-      @movies = Movie.all
+      @ratings = Movie.all_ratings
     end
+    
+    if (@sort_by == 'title') then
+      @movies = @movies.order(title: :asc)
+    elsif (@sort_by == 'release_date') then
+      @movies = @movies.order(release_date: :asc)
+    end
+
     @title_header = "title_header_class"
     @release_date_header = "release_date_header_class"
+    @all_ratings = Movie.all_ratings
   end
 
   def show
